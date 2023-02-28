5c17b25f9edc5194c3522748e0be926ad9ee2dd4
3P6V2D3 <phungvandung030623@gmail.com>
2023-02-28 16:27:18 +0700

    Update change file Lesson5.java

--- OOP_with_Java/Part_1/Lesson 5/Lesson5.java
+++ OOP_with_Java/Part_1/Lesson 5/Lesson5.java
@@ -23,11 +23,31 @@ class Triangle {
 
     public void checkTriangle() {
         if((a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a)) {
-            System.out.println("Tam giac co ba canh a, b, c la: Tam giac Vuong");
+            if(checkEdgs() == 1) {
+                System.out.println("Tam giac co ba canh a, b, c la: Tam giac Vuong can");
+            } else {
+                System.out.println("Tam giac co ba canh a, b, c la: Tam giac Vuong");
+            }
         } else if((a*a + b*b < c*c) || (a*a + c*c < b*b) || (b*b + c*c < a*a)) {
             System.out.println("Tam giac co ba canh a, b, c la: Tam giac Tu");
         } else {
-            System.out.println("Tam giac co ba canh a, b, c la: Tam giac Nhon");
+            if(checkEdgs() == 0) {
+                System.out.println("Tam giac co ba canh a, b, c la: Tam giac deu");
+            } else if(checkEdgs() == 1) {
+                System.out.println("Tam giac co ba canh a, b, c la: Tam giac can");
+            } else{
+                System.out.println("Tam giac co ba canh a, b, c la: Tam giac Nhon");
+            }
+        }
+    }
+
+    public int checkEdgs() {
+        if(a == b && a == c) {
+            return 0;
+        } else if(a == b || b == c || a == c) {
+            return 1;
+        } else {
+            return 2;
         }
     }
 
